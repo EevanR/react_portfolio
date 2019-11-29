c730c5a65bd636ec477aee23f0fa20a084114d42
Eevan <eevan.redon@gmail.com>
2019-11-29 12:10:52 +0100

    welcome sign locked in position

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Hello = () => {
     return (
         <div className="ui main container">
-            <h1 class="welcome">
+            <h1 class="centered">
               Welcome.
             </h1>
         </div>
