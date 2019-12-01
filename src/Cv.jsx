47150eb84e46b057652f29b8352939f26fdb0589
Eevan <eevan.redon@gmail.com>
2019-12-01 22:58:24 +0100

    css grey box

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -47,7 +47,7 @@ class Jobs extends Component {
                                     <img className="headshot" src="https://i.imgur.com/L38tMS2.jpg" />
                                 </div>
                             </div>
-                            <div className="centerText">
+                            <div id="greyBox" className="centerText">
                                 <h5 id="bannerText">My Working Life Until Now...</h5>
                             </div>
                             <div id="jobslist" className="ui stackable four column grid">
@@ -57,6 +57,7 @@ class Jobs extends Component {
                                 <div>
                                     <h5 id="bannerText">Education</h5>
                                     <p id="pDes">{info.education}</p>
+                                    <p id="pDes">{info.education2}</p>
                                 </div>
                             </div>
                         </div>
