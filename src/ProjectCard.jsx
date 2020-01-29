78674ca55416ef608bfc90d92f4fa2f1aa14391a
Eevan <eevan.redon@gmail.com>
2020-01-29 20:09:41 +0100

    update project cards, and include git hub link

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -6,7 +6,10 @@ const ProjectCard = (props) => {
     <>
       <div class="ui card">
           <a href={project.link} target="_blank"><img src={project.image} width="100%" height="300px"/></a>
-          <div class="bottom-left">{project.name}<br/>{project.description}</div>
+          <div class="bottom-left">
+            <h3>{project.name} <a href={project.gitHub} target="_blank"><i class="github icon"></i></a><br/></h3>
+            {project.description}
+          </div>
       </div>
     </>
   )
